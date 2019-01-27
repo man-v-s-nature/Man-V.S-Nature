c066411aebe888de8cf52999936222c73c876616
MadelynK <47043936+MadelynK@users.noreply.github.com>
2019-01-27 12:41:24 -0500

    final draft #1

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+/*
+	Tessellate by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:    [ '1281px',  '1680px' ],
+			normal:  [ '1001px',  '1280px' ],
+			narrow:  [ '737px',   '1000px' ],
+			mobile:  [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+
+const $form = $("#comment")
+$form.on("submit", function (event) {
+event.preventDefault()
+const name =$('#name').val()
+const email =$('#email').val()
+const message =$('#message').val()
+$('#comments').append(`
+<div>
+ ${name} - ${message}
+ <div>
+ `)
+
+})
+})(jQuery);
